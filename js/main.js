6c2bd4101f6f384f87360c3e4ab146666362c7bb
willian <joiltonwillian@gmail.com>
2022-09-08 22:50:31 -0300

    main update sandwich
    criando parte do sandwich e posicionando melhor algumas divs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 //window.onload(alert("Você caiu no conto da amoeba"))
+
+const btm = document.getElementById("sandwich");
+
+function toggleMenu() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle('active');
+}
+
+btm.addEventListener("click", toggleMenu);
