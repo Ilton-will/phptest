e63a046cef7999c748d6e56168705cbf1aad7284
Joilton <joiltonwillian@gmail.com>
2022-09-09 22:50:57 -0300

    update main page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 //window.onload(alert("Você caiu no conto da amoeba"))
 
 const btm = document.getElementById("sandwich");
+const config = document.getElementById("config-but");
+
+function actionBut() {
+    window.location.replace("config.html");
+}
 
 function toggleMenu() {
     const nav = document.getElementById("nav");
     nav.classList.toggle('active');
 }
 
+config.addEventListener("click", actionBut);
 btm.addEventListener("click", toggleMenu);
