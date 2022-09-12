3a9d7eaee2c877bdabdfba3fa626b7931d4d5e37
Joilton <joiltonwillian@gmail.com>
2022-09-12 03:43:56 -0300

    sandwich finalizado

--- js/main.js
+++ js/main.js
@@ -2,15 +2,41 @@
 
 const btm = document.getElementById("sandwich");
 const config = document.getElementById("config-but");
+const act = "active";
 
 function actionBut() {
     window.location.replace("config.html");
+    toggleMenu();
 }
 
 function toggleMenu() {
-    const nav = document.getElementById("nav");
-    nav.classList.toggle('active');
+    const listas = document.getElementById("list");
+    listas.classList.toggle(act);
+
 }
 
+function altersandwich(){
+    const san = document.getElementById("labsand");
+    san.classList.toggle(act);
+}
+
+
+function animatelinks() {
+
+    const links = document.querySelectorAll("li");
+    links.forEach((link, index) => {
+        link.style.animation 
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.2}s`);
+    });
+}
+
+function clickfunc(){
+    toggleMenu();
+    altersandwich();
+    animatelinks();
+}
+
+
 config.addEventListener("click", actionBut);
-btm.addEventListener("click", toggleMenu);
+btm.addEventListener("click", clickfunc);
